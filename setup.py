cab888e832a88e9727c79bc9363e118a9fed6e6d
gregor <wpumacay@gmail.com>
2023-08-04 16:39:10 -0500

    (chore): updating packaging method

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 import codecs
-import os.path
+import os
 
 from setuptools import setup
-
 
 # Version meaning (X.Y.Z)
 # X: Major version (e.g. vastly different scene, platform, etc)
@@ -12,45 +11,34 @@ from setuptools import setup
 
 def read(rel_path):
     here = os.path.abspath(os.path.dirname(__file__))
-    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
         return fp.read()
 
 
 def get_version(rel_path):
     for line in read(rel_path).splitlines():
-        if line.startswith('__version__'):
+        if line.startswith("__version__"):
             delim = '"' if '"' in line else "'"
             return line.split(delim)[1]
     else:
         raise RuntimeError("Unable to find version string.")
 
 
-core_requirements = [
-    "numpy",
-    "Pillow",
-    "pyquaternion",
-    "html-testRunner",
-    "natsort"
-]
-
-setup(name='rlbench',
-      version=get_version("rlbench/__init__.py"),
-      description='RLBench',
-      author='Stephen James',
-      author_email='slj12@ic.ac.uk',
-      url='https://www.doc.ic.ac.uk/~slj12',
-      install_requires=core_requirements,
-      packages=[
-            'rlbench',
-            'rlbench.backend',
-            'rlbench.action_modes',
-            'rlbench.tasks',
-            'rlbench.task_ttms',
-            'rlbench.robot_ttms',
-            'rlbench.sim2real',
-            'rlbench.assets',
-            'rlbench.gym'
-      ],
-      package_data={'': ['*.ttm', '*.obj', '**/**/*.ttm', '**/**/*.obj'],
-                    'rlbench': ['task_design.ttt']},
-      )
+setup(
+    version=get_version("rlbench/__init__.py"),
+    packages=[
+        "rlbench",
+        "rlbench.backend",
+        "rlbench.action_modes",
+        "rlbench.tasks",
+        "rlbench.task_ttms",
+        "rlbench.robot_ttms",
+        "rlbench.sim2real",
+        "rlbench.assets",
+        "rlbench.gym",
+    ],
+    package_data={
+        "": ["*.ttm", "*.obj", "**/**/*.ttm", "**/**/*.obj"],
+        "rlbench": ["task_design.ttt"],
+    },
+)
